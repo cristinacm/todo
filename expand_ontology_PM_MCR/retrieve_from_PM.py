b497085b9c6c8bb5a1a8a3066aadd34f43651c15
Cristina <caceta@tekniker.es>
2020-09-28 11:48:43 +0200

    Removed prefixes from PredicateMatrix. Corrected SPARQL header+ending.
    Former-commit-id: b552ac47d9e196b699759256e12bcda057afb5b8

--- expand_ontology_PM_MCR/retrieve_from_PM.py
+++ expand_ontology_PM_MCR/retrieve_from_PM.py
@@ -1,5 +1,6 @@
 
-
+# pred_matrix = (open("PredicateMatrix_v1_3.tsv")).read()
+# pred_matrix_split = pred_matrix.split("\n")
 # print(pred_matrix_split)
 
 def create_out_dict(out, frame_col, LU_col, ILI_col):
@@ -46,17 +47,17 @@ def extract_ili_predmatrix_onlyoneframe(pred_matrix_split, frame, LUs, SUMOs):
 			# print("ok")
 			if LU_col not in ilis_lu_PM.keys():
 				# print("ok")
-				ilis_lu_PM[LU_col] = [ILI_col]
+				ilis_lu_PM[LU_col[3:-2]] = [ILI_col[4:]]
 				# print(ilis_lu_PM)
-			elif ILI_col not in ilis_lu_PM[LU_col]:
-				ilis_lu_PM[LU_col].append(ILI_col)
+			elif ILI_col not in ilis_lu_PM[LU_col[3:]]:
+				ilis_lu_PM[LU_col[3:-2]].append(ILI_col[4:])
 			
 			# ilis_PM.append(ILI_col)
 	# print(ilis_lu_PM)
 		# if len(ilis_PM) < 1:
 			# out[frame] = {}
 				# out[frame][lu]
-		out[frame] = ilis_lu_PM
+		out[frame[3:]] = ilis_lu_PM
 		# print(out)
 	return(out)
 
@@ -88,16 +89,16 @@ def extract_ili_predmatrix_morethanoneframe(pred_matrix_split, args):
 			# print("ok")
 		if frame_col in args.keys():
 			if len(args[frame_col]["LUs"]) < 1 and len(args[frame_col]["SUMOs"]) < 1:
-					out = create_out_dict(out, frame_col, LU_col, ILI_col[4:])
+					out = create_out_dict(out, frame_col[3:], LU_col[3:-2], ILI_col[4:])
 			if len(args[frame_col]["LUs"]) < 1:
 				if SUMO_col in args[frame_col]["SUMOs"]:
-					out = create_out_dict(out, frame_col, LU_col, ILI_col[4:])
+					out = create_out_dict(out, frame_col[3:], LU_col[3:-2], ILI_col[4:])
 			elif len(args[frame_col]["SUMOs"]) < 1:
-				if LU_col in args[frame_col]["LUs"]:
-					out = create_out_dict(out, frame_col, LU_col, ILI_col[4:])
+				if LU_col[3:-2] in args[frame_col]["LUs"]:
+					out = create_out_dict(out, frame_col[3:], LU_col[3:-2], ILI_col[4:])
 			else:
 				if SUMO_col in args[frame_col]["SUMOs"] and LU_col in args[frame_col]["LUs"]:
-					out = create_out_dict(out, frame_col, LU_col, ILI_col[4:])
+					out = create_out_dict(out, frame_col[3:], LU_col[3:-2], ILI_col[4:])
 			
 			
 			
@@ -134,3 +135,4 @@ def extract_ili_predmatrix_morethanoneframe(pred_matrix_split, args):
 # print(extract_ili_predmatrix_onlyoneframe(pred_matrix_split, frame, LUs, SUMOs))
 
 # args = {"fn:Motion": {"LUs": ["fn:go.v", "fn:move.v"], "SUMOs": ["mcr:Motion", "mcr:DirectionChange", "mcr:BodyMotion"]}, "fn:Taking": {"LUs": ["fn:take.v", "NULL"], "SUMOs": ["mcr:Guiding", "mcr:Transfer"]}}
+# print(extract_ili_predmatrix_morethanoneframe(pred_matrix_split, args))
